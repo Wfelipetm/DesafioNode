2ac3b163afec418c2d9045b72faa031b1c55fdff
Wallace <wfelipetm@gmail.com>
2023-07-25 11:24:33 -0300

    .

--- 023/023.js
+++ 023/023.js
@@ -5,29 +5,19 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-function leiaValor(mensagem) {
+function leiaValorPositivo(mensagem) {
   return new Promise((resolve) => {
     rl.question(mensagem, (valor) => {
-      resolve(valor);
+      resolve(parseFloat(valor));
     });
   });
 }
 
-function leiaValorPositivo(mensagem) {
-  return new Promise(async (resolve) => {
-    let valor = await leiaValor(mensagem);
-    valor = parseFloat(valor);
-    while (isNaN(valor) || valor <= 0) {
-      console.log("Valor inválido. Tente novamente.");
-      valor = await leiaValor(mensagem);
-      valor = parseFloat(valor);
-    }
-    resolve(valor);
-  });
-}
+function calcularSalarioBruto(horas_trabalhadas, dependente) {
+  const valor_hora = 23.0;
+  const adicional_dependente = 34.0;
 
-function calcularSalarioBruto(dias_trabalhados, dependente) {
-  return 23.0 * dias_trabalhados + dependente * 34.0;
+  return valor_hora * horas_trabalhadas + adicional_dependente * dependente;
 }
 
 function calcularDescontoINSS(salario_bruto) {
@@ -41,21 +31,21 @@ function calcularDescontoIR(salario_bruto) {
 async function calcularSalario() {
   console.log("--------------------------------------------");
 
-  const nome_funcionario = await leiaValor("Digite o nome do funcionario: ");
-  const dias_trabalhados = await leiaValorPositivo("Dias trabalhados: ");
+  const nome_funcionario = await leiaValorPositivo("Digite o nome do funcionário: ");
+  const horas_trabalhadas = await leiaValorPositivo("Horas trabalhadas: ");
   const dependente = await leiaValorPositivo("Quantos dependentes: ");
 
-  const salario_bruto = calcularSalarioBruto(dias_trabalhados, dependente);
+  const salario_bruto = calcularSalarioBruto(horas_trabalhadas, dependente);
   const desconto_inss = calcularDescontoINSS(salario_bruto);
   const desconto_ir = calcularDescontoIR(salario_bruto);
   const salario_liquido = salario_bruto - desconto_inss - desconto_ir;
 
   console.log("-------------------------------------------");
-  console.log("Nome do funcionario: " + nome_funcionario);
+  console.log("Nome do funcionário: " + nome_funcionario);
   console.log("Salário bruto: R$ " + salario_bruto.toFixed(2));
   console.log("Desconto INSS: R$ " + desconto_inss.toFixed(2));
   console.log("Desconto IR: R$ " + desconto_ir.toFixed(2));
-  console.log("Salário Liquido: R$ " + salario_liquido.toFixed(2));
+  console.log("Salário Líquido: R$ " + salario_liquido.toFixed(2));
   console.log("-------------------------------------------");
 
   rl.close();
